a264d75abd17afba69153ceb5dfe723631295418
Spencer King <spencer@spencertking.com>
2021-07-11 19:14:48 -0500

    Start implementing validation

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,22 @@ fn main() {
     }
     
     let file = File::open(in_file).unwrap();
-    let reader = BufReader::new(file);
+    //let reader = BufReader::new(file);
 
+    validation::validate(&file);
+
+    /*
     let mut seqInd = 1;
     for (index, line) in reader.lines().enumerate() {
 	let mut line = line.unwrap();
 	//println!("{}", line);
 
-	validation::validate(&line, &seqInd);
+	validation::validate(&line, index, seqInd);
 	
 	seqInd = seqInd + 1;
 	if seqInd > 4 {
 	    seqInd = 1;
 	}
     }
+     */
 }
--- src/validation.rs
+++ src/validation.rs
@@ -1,7 +1,44 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+// TODO: Finish implementing validation criteria
 // Criteria: https://genome.sph.umich.edu/wiki/FastQ_Validation_Criteria
 
-pub fn validate(line: &str, index: &u8) {
-    println!("Validate");
+pub fn validate(file: &File) {
+//pub fn validate(line: &str, lineNumber: usize, seqIndex: usize) {
+    //println!("{}", lineNumber);
+    //println!("{}", seqIndex);
+    //println!("Validate");
+
+    let reader = BufReader::new(file);
+
+    let mut seqIndex = 1;
+    for (lineNumber, line) in reader.lines().enumerate() {
+	let mut line = line.unwrap();
+	    
+	if seqIndex == 1 {
+	    if line.chars().count() < 2 {
+		println!("Error: Line number {} is too short", lineNumber);
+	    }
+
+	    if line.chars().next().unwrap() != '@' {
+		println!("Error: Line number {} must start with an @", lineNumber);
+	    }
+	} else if seqIndex == 2 {
+
+	} else if seqIndex == 3 {
+	    if line.chars().next().unwrap() != '+' {
+		println!("Error: Line number {} must start with a +", lineNumber);
+	    }
+	} else if seqIndex == 4 {
+
+	}
+
+	seqIndex = seqIndex + 1;
+	if seqIndex > 4 {
+	    seqIndex = 1;
+	}
+    }
 }
 
 pub fn quality_scores() {
